5a5e6110d0ab7bc91b2afc2d25a4080973d26ed8
Igor Klevzhits <klevzhits94@icloud.com>
2025-02-04 07:39:38 +0300

    Create UI

--- FTAM/Extontions + UIKit.swift
+++ FTAM/Extontions + UIKit.swift
@@ -5,4 +5,23 @@
 //  Created by Игорь Клевжиц on 04.02.2025.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    convenience init(nameImage: String) {
+        self.init(image: UIImage(named: nameImage))
+        self.contentMode = .scaleAspectFit
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension UIButton {
+    convenience init(color: UIColor, emoji: String) {
+        self.init(type: .system)
+        self.backgroundColor = color
+        self.layer.cornerRadius = 20
+        self.setTitle(emoji, for: .normal)
+        self.titleLabel?.font = .systemFont(ofSize: 35)
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
